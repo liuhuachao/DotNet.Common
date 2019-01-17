36dd6754815b111790ee5f93b3ca14b470c9c2fa
liuhuachao <liuhuachao@foxmail.com>
2019-01-17 17:06:09 +0800

    feat:添加配置文件操作类

--- DotNet.Common/CacheHelper.cs
+++ DotNet.Common/CacheHelper.cs
@@ -3,73 +3,87 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
+using System.Web.Caching;
 
 namespace DotNet.Common
 {
     /// <summary>
     /// 缓存操作类
     /// </summary>
+    /// <summary>
+    /// 缓存帮助类
+    /// </summary>
     public class CacheHelper
     {
         /// <summary>
-        /// 获取数据缓存
+        /// 获取缓存对象
         /// </summary>
-        /// <param name="CacheKey">键</param>
-        public static object GetCache(string CacheKey)
+        /// <param name="key">缓存Key</param>
+        /// <returns>object对象</returns>
+        public static object Get(string key)
         {
-            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
-            return objCache[CacheKey];
-        }
-
-        /// <summary>
-        /// 设置数据缓存
-        /// </summary>
-        public static void SetCache(string CacheKey, object objObject)
-        {
-            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
-            objCache.Insert(CacheKey, objObject);
-        }
-
-        /// <summary>
-        /// 设置数据缓存
-        /// </summary>
-        public static void SetCache(string CacheKey, object objObject, TimeSpan Timeout)
-        {
-            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
-            objCache.Insert(CacheKey, objObject, null, DateTime.MaxValue, Timeout, System.Web.Caching.CacheItemPriority.NotRemovable, null);
-        }
-
-        /// <summary>
-        /// 设置数据缓存
-        /// </summary>
-        public static void SetCache(string CacheKey, object objObject, DateTime absoluteExpiration, TimeSpan slidingExpiration)
-        {
-            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
-            objCache.Insert(CacheKey, objObject, null, absoluteExpiration, slidingExpiration);
-        }
-
-        /// <summary>
-        /// 移除指定数据缓存
-        /// </summary>
-        public static void RemoveAllCache(string CacheKey)
-        {
-            System.Web.Caching.Cache _cache = HttpRuntime.Cache;
-            _cache.Remove(CacheKey);
-        }
-
-        /// <summary>
-        /// 移除全部缓存
-        /// </summary>
-        public static void RemoveAllCache()
-        {
-            System.Web.Caching.Cache _cache = HttpRuntime.Cache;
-            IDictionaryEnumerator CacheEnum = _cache.GetEnumerator();
-            while (CacheEnum.MoveNext())
+            if (string.IsNullOrEmpty(key))
             {
-                _cache.Remove(CacheEnum.Key.ToString());
+                return null;
             }
+            return HttpContext.Current.Cache.Get(key);
+        }
+
+        /// <summary>
+        /// 获取缓存对象
+        /// </summary>
+        /// <typeparam name="T">T对象</typeparam>
+        /// <param name="key">缓存Key</param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            object obj = Get(key);
+            return obj == null ? default(T) : (T)obj;
+        }
+
+        /// <summary>
+        /// 插入缓存对象
+        /// </summary>
+        /// <param name="key">缓存Key</param>
+        /// <param name="obj">object对象</param>
+        public static void Insert(string key, object obj)
+        {
+            HttpContext.Current.Cache.Insert(key, obj);
+        }
+
+        /// <summary>
+        /// 插入缓存对象
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <param name="fileName">依赖项文件名</param>
+        public static void Insert(string key, object obj,string fileName)
+        {
+            CacheDependency dep = new CacheDependency(fileName);  //创建缓存依赖项
+            HttpContext.Current.Cache.Insert(key, obj, dep);
+        }
+
+        /// <summary>
+        /// 插入缓存对象
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <param name="absoluteExpiration">绝对过期时间</param>
+        /// <param name="slidingExpiration">相对过期时间</param>
+        public static void Insert(string key, object obj,string fileName, DateTime absoluteExpiration, TimeSpan slidingExpiration)
+        {
+            CacheDependency dep = new CacheDependency(fileName);  //创建缓存依赖项
+            HttpContext.Current.Cache.Insert(key, obj, null, absoluteExpiration, slidingExpiration);
+        }
+
+        /// <summary>
+        /// 移除缓存对象
+        /// </summary>
+        /// <param name="key">缓存Key</param>
+        public static void Remove(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
         }
     }
 }
---
+++ DotNet.Common/ConfigHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Common
+{
+    /// <summary>
+    /// 配置文件(AppSettings)操作类
+    /// </summary>
+    public sealed class ConfigHelper
+    {
+        public static bool GetConfigBool(string key)
+        {
+            bool flag = false;
+            string configString = GetConfigString(key);
+            if ((configString != null) && (string.Empty != configString))
+            {
+                try
+                {
+                    flag = bool.Parse(configString);
+                }
+                catch (FormatException)
+                {
+                }
+            }
+            return flag;
+        }
+
+        public static string GetConfigString(string key)
+        {
+            string cacheKey = "AppSettings-" + key;
+            object cache = CacheHelper.Get(cacheKey);
+            if (cache == null)
+            {
+                try
+                {
+                    cache = ConfigurationManager.AppSettings[key];
+                    if (cache != null)
+                    {
+                        CacheHelper.Insert(cacheKey, cache,"", DateTime.UtcNow.AddMinutes(180.0), TimeSpan.Zero);
+                    }
+                }
+                catch
+                {
+                }
+            }
+            return cache.ToString();
+        }
+    }
+}
