3e5cd308ad05793631fd9134be5ad8f4598c445d
liuhuachao <liuhuachao@foxmail.com>
2019-01-17 14:24:03 +0800

    feat:新增二维码帮助类

---
+++ DotNet.Common/QRCodeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QRCoder;
+
+namespace DotNet.Common
+{
+    /// <summary>
+    /// 二维码帮助类
+    /// </summary>
+    public class QRCodeHelper
+    {
+        public static MemoryStream CreateQRCode(string text)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            var ms = new System.IO.MemoryStream();
+            qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            return ms;
+        }
+    }
+}
