f15c6c804600870ff9c089e1da834794336753ca
liuhuachao <liuhuachao@foxmail.com>
2019-01-18 10:54:25 +0800

    feat:新增DataTable 操作类

---
+++ DotNet.Common/DataTableHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Common
+{
+    /// <summary>
+    /// DataTable 操作类
+    /// </summary>
+    public class DataTableHelper
+    {
+        /// <summary>
+        /// DataTable 转 XML
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string DataTableToXML(DataTable dt)
+        {
+            if (dt != null)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    System.IO.StringWriter writer = new System.IO.StringWriter();
+                    dt.WriteXml(writer);
+                    return writer.ToString();
+                }
+            }
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// DataTable转为对象LIST
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns>数据行是对象的类，类的属性与数据字段一致</returns>
+        public static IList DataTableToIList<T>(DataTable dt)
+        {
+            // 定义集合    
+            string tempName = "";
+            IList list = new List<T>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                T obj = Activator.CreateInstance<T>();
+                //获得此模型的公共属性
+                PropertyInfo[] propertys = obj.GetType().GetProperties();
+                foreach (PropertyInfo pi in propertys)
+                {
+                    tempName = pi.Name;
+                    //检查DataTable是否包含此列
+                    if (dt.Columns.Contains(tempName))
+                    {
+                        //判断此属性是否有Setter    
+                        if (!pi.CanWrite) continue;
+                        object value = dr[tempName];
+                        if (value != DBNull.Value)
+                        {
+                            pi.SetValue(obj, value, null);
+                        }
+                    }
+                }
+                list.Add(obj);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// DataRow 转 实体类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public static T DataRowToModel<T>(DataRow dr)
+        {
+            T model = Activator.CreateInstance<T>();
+            foreach (PropertyInfo pi in model.GetType().GetProperties(BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance))
+            {
+                if (!IsNullOrDBNull(dr[pi.Name]))
+                {
+                    pi.SetValue(model, HackType(dr[pi.Name], pi.PropertyType), null);
+                }
+            }
+            return model;
+        }
+        public static object HackType(object value, Type conversionType)
+        {
+            if (conversionType.IsGenericType && conversionType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                if (value == null) return null;
+                System.ComponentModel.NullableConverter nullableConverter = new System.ComponentModel.NullableConverter(conversionType);
+                conversionType = nullableConverter.UnderlyingType;
+            }
+            return Convert.ChangeType(value, conversionType);
+        }
+        public static bool IsNullOrDBNull(object obj)
+        {
+            return ((obj is DBNull) || string.IsNullOrEmpty(obj.ToString())) ? true : false;
+        }
+
+        /// <summary>
+        /// DataRow 转 HashTable
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public static Hashtable DataRowToHashTable(DataRow dr)
+        {
+            Hashtable ht = new Hashtable(dr.ItemArray.Length);
+            foreach (DataColumn dc in dr.Table.Columns)
+            {
+                ht.Add(dc.ColumnName, dr[dc.ColumnName]);
+            }
+            return ht;
+        }
+
+    }
+}
