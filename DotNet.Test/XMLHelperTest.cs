78f6b033d649efcac721aaf3720433bc807a5e66
liuhuachao <liuhuachao@foxmail.com>
2019-03-27 16:22:40 +0800

    test:测试xml转DataSet

---
+++ DotNet.Test/XMLHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNet.Common;
+
+namespace DotNet.Test
+{
+    [TestClass]
+    public class XMLHelperTest
+    {
+        [TestMethod]
+        public void TestXMLToDataSet()
+        {
+            var ds = DataTableHelper.XMLToDataSet("c:/BOM.xml");
+        }
+    }
+}
