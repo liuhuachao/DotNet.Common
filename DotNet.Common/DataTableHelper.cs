6a8dc2b32e53d42bbe30e343192b3058fca88611
liuhuachao <liuhuachao@foxmail.com>
2019-04-04 14:15:10 +0800

    feat:新增获取 Oracle In Where 条件的方法

--- DotNet.Common/DataTableHelper.cs
+++ DotNet.Common/DataTableHelper.cs
@@ -166,6 +166,43 @@ namespace DotNet.Common
         }
 
         /// <summary>
+        /// 获取 in where 条件
+        /// 针对 Oracle 查询中 in 参数超过1000的处理方法
+        /// </summary>
+        /// <param name="dt">数据源 DataTable</param>
+        /// <param name="columnName">DataTable 列名</param>
+        /// <param name="keyName">数据库表列名</param>
+        /// <returns></returns>
+        public static string GetInWhere(DataTable dt, string columnName, string keyName)
+        {
+            var where = string.Empty;
+            var whereKey = string.Empty;
+            dt = dt.DefaultView.ToTable(true, columnName);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                var key = Convert.ToString(dt.Rows[i][columnName]);
+                if (string.IsNullOrEmpty(key))
+                {
+                    continue;
+                }
+                if (i == dt.Rows.Count - 1)
+                {
+                    whereKey += $"'{key}'";
+                }
+                else if (i > 0 && i % 999 == 0)
+                {
+                    whereKey += $"'{key}') OR {keyName} IN (";
+                }
+                else
+                {
+                    whereKey += $"'{key}',";
+                }
+            }
+            where += $" {keyName} IN ({whereKey}) ";
+            return where;
+        }
+
+        /// <summary>
         /// IList 转 DataTable
         /// </summary>
         /// <typeparam name="T"></typeparam>
