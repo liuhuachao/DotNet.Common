00673dcd7463fe1526d03796e33fb24d7a30c5a0
liuhuachao <liuhuachao@foxmail.com>
2022-01-05 14:58:09 +0800

    feat：新增 Word 文档帮助类

---
+++ DotNet.Common/WordHelper.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NPOI.XWPF.UserModel;
+
+namespace DotNet.Common
+{
+    /// <summary>
+    /// Word 文档帮助类
+    /// </summary>
+    public class WordHelper
+    {
+        /// <summary>
+        /// 通过替换模板占位符生成新文档
+        /// </summary>
+        /// <param name="modelFilePath">模板文件物理路径</param>
+        /// <param name="outFilePath">输出文件物理路径</param>
+        /// <param name="paramDic">参数字典</param>
+        /// <param name="placeholder">占位符</param>
+        public static void ExportByModelFile(string modelFilePath, string outFilePath, Dictionary<string, string> paramDic, string placeholder = "#")
+        {
+            using (FileStream docFile = new FileStream(modelFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // 通过模板加载文档
+                XWPFDocument doc = new XWPFDocument(docFile);
+
+                // 遍历段落                  
+                foreach (var para in doc.Paragraphs)
+                {
+                    ReplaceKey(para, paramDic, placeholder);
+                }
+
+                // 遍历表格      
+                var tables = doc.Tables;
+                foreach (var table in tables)
+                {
+                    LoopTable(table, paramDic, placeholder);
+                }
+
+                // 创建目录
+                var path = System.IO.Path.GetDirectoryName(outFilePath);
+                if (!System.IO.Directory.Exists(path))
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+
+                // 生成文件
+                FileStream outStream = new FileStream(outFilePath, FileMode.CreateNew);
+                doc.Write(outStream);
+                outStream.Close();
+            }
+        }
+
+        /// <summary>
+        /// 遍历表格
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="param"></param>
+        /// <param name="placeHolder">占位符</param>
+        private static void LoopTable(XWPFTable table, Dictionary<string, string> param, string placeHolder)
+        {
+            foreach (var row in table.Rows)
+            {
+                foreach (var cell in row.GetTableCells())
+                {
+                    if (cell.Tables != null && cell.Tables.Any())
+                    {
+                        foreach (var tab in cell.Tables)
+                        {
+                            LoopTable(tab, param, placeHolder);
+                        }
+                    }
+                    foreach (var para in cell.Paragraphs)
+                    {
+                        ReplaceKey(para, param, placeHolder);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 替换占位符
+        /// </summary>
+        /// <param name="paragraph">段落</param>
+        /// <param name="paramDic">参数字典</param>
+        /// <param name="placeHolder">占位符</param>
+        private static void ReplaceKey(XWPFParagraph paragraph, Dictionary<string, string> paramDic, string placeHolder)
+        {
+            string text = paragraph.ParagraphText;
+
+            if (null == text || text.Length <= 3) return;
+
+            foreach (var entry in paramDic)
+            {
+                if (text.Contains(placeHolder + entry.Key + placeHolder))
+                {
+                    paragraph.ReplaceText(placeHolder + entry.Key + placeHolder, entry.Value ?? "");
+                }
+            }
+
+            var checkedStr = "!";
+            var unCheckedStr = Convert.ToChar(0x00A3).ToString();
+
+            var runs = paragraph.Runs;
+            for (int i = 0; i < runs.Count; i++)
+            {
+                var run = runs[i];
+                var runText = run.ToString();
+
+                // 特殊字符  ☑ 0x0052 未选中 0x00A3
+                if (runText.Contains(checkedStr) || runText.Contains(unCheckedStr))
+                {
+                    run.ReplaceText(checkedStr, "R");
+                    run.ReplaceText(unCheckedStr, "");
+                    run.FontFamily = "Wingdings 2";
+                }
+            }
+
+        }
+    }
+
+}
---
+++ DotNet.Test/WordHelperTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using DotNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Common.Test
+{
+    public class WordHelperTest
+    {
+        [Fact()]
+        public void TestExportByModelFile()
+        {
+            string modelFilePath = AppContext.BaseDirectory + "\\Files\\真空断路器出厂检验报告样例.docx";
+            string outFilePath = AppContext.BaseDirectory + "\\Files\\真空断路器出厂检验报告样例-new.docx";
+
+            Dictionary<string, string> paramDic = new Dictionary<string, string>();
+            paramDic.Add("ProductModel", "NPV12T1231D22E");
+            paramDic.Add("ProductCode", "PV181266");
+            paramDic.Add("ReportCode", "2018-09-16Z-001");
+
+            var checkedStr = "!";
+            var unCheckedStr = Convert.ToChar(0x00A3).ToString(); // "£"
+            paramDic.Add("p1.2A", unCheckedStr);
+            paramDic.Add("p1.2B", checkedStr);
+            paramDic.Add("p1.2C", unCheckedStr);
+            paramDic.Add("p1.2D", unCheckedStr);
+
+            WordHelper.ExportByModelFile(modelFilePath, outFilePath, paramDic);
+
+            //Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
